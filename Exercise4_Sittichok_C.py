ba0534c1722716335fc370582f516898fa074302
tidesittichok <40417761+tidesittichok@users.noreply.github.com>
2022-08-31 16:26:05 +0700

    Exercise4

--- Exercise4_Sittichok_C.py
+++ Exercise4_Sittichok_C.py
@@ -1,1 +1,12 @@
-FoundationEnglish = 90.05
+ScoreFoundationEnglish = 90.05
+ScoreGeneralBusiness = 95.25
+ScoreIntroToComputer = 97.50
+ScoreComputerProgramming = 92.75
+
+print("--- Your Score ---")
+print("Foundation English :",ScoreFoundationEnglish)
+print("General Business :",ScoreGeneralBusiness)
+print("Introduction to Computer Systems :",ScoreIntroToComputer)
+print("Computer Programming :",ScoreComputerProgramming)
+
+print()
