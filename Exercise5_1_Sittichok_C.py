0318630c41f27bfcc8379407af7668a81691f221
tidesittichok <40417761+tidesittichok@users.noreply.github.com>
2022-09-01 10:34:26 +0700

    Update Exercise 5.1

--- Exercise5_1_Sittichok_C.py
+++ Exercise5_1_Sittichok_C.py
@@ -1,1 +1,6 @@
-
+num1 = int(input("input first number : "))
+num2 = int(input("input second number : "))
+print(num1,"+",num2,"=",(num1+num2))
+print(num1,"-",num2,"=",(num1-num2))
+print(num1,"*",num2,"=",(num1*num2))
+print(num1,"/",num2,"=",(num1/num2))
